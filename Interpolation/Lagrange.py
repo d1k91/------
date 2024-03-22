14d0b4a260c790645785148edce7a863745c3127
Vadim Glinskiy <v.glinskiy@.com>
2024-03-22 16:57:01 +0700

    Наконецтаки блять я это сделал

--- Interpolation/Lagrange.py
+++ Interpolation/Lagrange.py
@@ -2,17 +2,19 @@ import matplotlib.pyplot as plt
 import math
 
 data = [2.56]
+#ax = plt.gca()
+#ax.spines['bottom'].set_position('center')
+#ax.spines['top'].set_visible(False)
+#ax.spines['right'].set_visible(False)
+
 
 def main():
     x, y = init()
-    print(Lagrange(x, y, 2.56))
-    print(Eytken(x, y, 5, 2.56))
-    print(newton_interpolation(2.56, x,y))
-    print(newton(2.56, x, y))
-    for i in range(7):
-        print(i, math.sin(i))
-    for_graph(x,y, 4)
-    
+    print(Lagrange(x, y, data[0]))
+    print(Eytken(x, y, len(x)-1, data[0]))
+    print(newton_interpolation(data[0], x,y))
+    print(newton(data[0], x, y))
+    graph(x,y)
 
 def print_tabl(delta):
     for i in range(len(delta)):
@@ -35,33 +37,33 @@ def divided_differences(x_values, y_values):
     #print_tabl(F)
     return F[0]
 
-def newton_interpolation(x, x_values, y_values):
-    coeffs = divided_differences(x_values, y_values)
-    n = len(x_values)
+def newton_interpolation(point, x, y):
+    coeffs = divided_differences(x, y)
+    n = len(x)
     interpolation = coeffs[0]
     for i in range(1, n):
         term = coeffs[i]
         for j in range(i):
-            term *= (x - x_values[j])
+            term *= (point - x[j])
         interpolation += term
     return round(interpolation, 5)
 
-def newton(x, x_val, y_val):
-    n = len(x_val)
+def newton(point, x, y):
+    n = len(x)
     F = [[None] * n for _ in range(n)]
 
     for i in range(n):
-        F[i][0] = y_val[i]
+        F[i][0] = y[i]
 
     for j in range(1, n):
         for i in range(n - j):
-            F[i][j] = (F[i + 1][j - 1] - F[i][j - 1]) / (x_val[i + j] - x_val[i])
+            F[i][j] = (F[i + 1][j - 1] - F[i][j - 1]) / (x[i + j] - x[i])
     #print_tabl(F)
     interpolation = F[0][0]
     for j in range(1, n):
         term = F[0][j]
         for i in range(j):
-            term *= (x - x_val[i])
+            term *= (point - x[i])
         interpolation += term
 
     return round(interpolation, 5)
@@ -91,65 +93,52 @@ def Eytken(x, y, n, point):
 
     
 def init():
-    with open("D:/vich/Interpolation/nums.txt", "r") as f:
+    with open("F:/4 семак/вычмат/VichMat/------/Interpolation/nums.txt", "r") as f:
         nums = f.readlines()
         n = len(nums)
         x = [0] * n
         y = [0] * n
-        print(x)
         for i in range(n):
             raw = nums[i].split()
             x[i] = float(raw[0])
             y[i] = float(raw[1])
     return x, y
-            
-            
-def for_graph(x,y, n = None):
-    # if n == None:
-    #     for i in data:
-    #         y.append(Lagrange(x,y,i))
-    #         x.append(i)
-        
-    #     for j in range(len(x)):
-    #         if x[j] in data:
-    #             plt.scatter(x[j], y[j], c = "red", s = 15)
-    #         else:
-    #             plt.scatter(x[j], y[j], c = "blue", s = 15)
-    #     plt.show()
-        
-        
-    # else:
-    #     for i in data:
-    #         y.append(Eytken(x,y,n,i))
-    #         x.append(i)
-            
-    #     y_r = y[0:n]
-    #     x_r = x[0:n]
-    #     x_r.append(x[-1])
-    #     y_r.append(y[-1])
-        
-    #     for j in range(n+1):
-    #         if x_r[j] in data:
-    #             plt.scatter(x_r[j], y_r[j], c = "red", s = 15)
-    #         else:
-    #             plt.scatter(x_r[j], y_r[j], c = "blue", s = 15)
-    #     plt.show()
-    
-    for i in data:
-        y.append(newton(i,x,y))
-        x.append(i)
-    x.sort()
-    y.sort()
-    y_r=[0]*len(y)
-    x_r=[0]*len(x)
-    for i in range(1, len(y)+1):
-        x_r[i-1] = i
-        y_r[i-1] = math.sin(i)
-    plt.plot(x_r, y_r, color='red')
-    plt.plot(x, y, color='blue')
-    plt.show()
                 
+                
+def graph(x,y):
+    x_r = [0]*(int(x[-1])*10)
+    y_r = [0]*(int(x[-1])*10)
+    x_e = x_l = x_n1 = x_n2 = x
+    y_e = y_l = y_n1 = y_n2= y
+    for i in range(int(x[-1])*10):
+        x_r[i] = (i+1)/10
+        y_r[i] = math.sin((i+1)/10)
+    
+    for i in range(1,len(x)):
+        if x[i] > data[0]:
+            y_l = y_l[0:i] + [Lagrange(x_l, y_l, data[0])] + y_l[i:]
+            x_l = x_l[0:i] + [data[0]] + x_l[i:]
+            
+            y_e = y_e[0:i] + [Eytken(x_e, y_e, len(x_e)-1, data[0])] + y_e[i:]
+            x_e = x_e[0:i] + [data[0]] + x_e[i:]
+            
+            y_n1 = y_n1[0:i] + [newton(data[0], x_l, y_l)] + y_n1[i:]
+            x_n1 = x_n1[0:i] + [data[0]] + x_n1[i:]
+            
+            y_n2 = y_n2[0:i] + [newton_interpolation(data[0], x_n2, y_n2)] + y_n2[i:]
+            x_n2 = x_n2[0:i] + [data[0]] + x_n2[i:]
+            break
+                
+    
+    #plt.plot(x_l,y_l,"bo", linewidth=2.0, label = "Lagrange")
+    #plt.plot(x_e,y_e,"yo", linewidth=2.0, label = "Eytken")
+    #plt.plot(x_n1,y_n1,"go", linewidth=2.0, label = "Newton1")
+    #plt.plot(x_n2,y_n2, "co", linewidth=2.0, label = "Newton2")
+    plt.plot(x_r,y_r,"r--", linewidth=2.0, label = "Function")
+    plt.legend()
+    plt.show()
 
+    
 
 if __name__ == "__main__":
     main()
