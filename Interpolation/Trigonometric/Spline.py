9d7fcb63dc508fa5e7aec520c14ddfead1c10aca
Vadim Glinskiy <v.glinskiy@icloud.com>
2024-03-23 12:32:36 +0700

    Сплайн жестко да иу

--- Interpolation/Trigonometric/Spline.py
+++ Interpolation/Trigonometric/Spline.py
@@ -2,6 +2,7 @@ import Gauss
 import matplotlib.pyplot as plt
 
 def main():
+    global x
     x,y = init()
     print(x, y, "\n")
     C,d = find_C(x,y)
@@ -12,16 +13,18 @@ def main():
     M = [0] + M + [0]
     S = find_s(x,y,M,1.1)
     graph(x,y,M)
-    
+
+
+def h(i):
+    return x[i]-x[i-1]   
 
 def graph(x,y,m):
     plt.plot(x, y)
-    x_r = [0]*(int(x[-1])*10)
-    y_r = [0]*(int(x[-1])*10)
-    for i in range(10, int(x[-1])*10):
-        x_r[i] = i/10
-        y_r[i] = find_s(x, y, m, i/10)
-        #print(x_r[i], y_r[i])
+    x_r = [0]*(int(x[-1])*10-19)
+    y_r = [0]*(int(x[-1])*10-19)
+    for i in range(20, int(x[-1])*10+1):
+        x_r[i-20] = i/10
+        y_r[i-20] = find_s(x, y, m, i/10)
     plt.plot(x_r, y_r)
     plt.show()
     
@@ -34,39 +37,35 @@ def find_s(x,y,M, point):
         if x[k] > point:
             i = k
             break
-    s += M[i-1]*((x[i]-point)**3)/(6*(x[i]-x[i-1]))
-    s += M[i]*((point-x[i-1])**3)/(6*(x[i]-x[i-1]))
-    s += (y[i-1] - (M[i-1]*(x[i]-x[i-1])**2)/6)*((x[i]-point)/(x[i]-x[i-1]))
-    s += (y[i] - (M[i]*(x[i]-x[i-1])**2)/6)*((point-x[i-1])/(x[i]-x[i-1]))
+    s += M[i-1]*((x[i]-point)**3)/(6*h(i))
+    s += M[i]*((point-x[i-1])**3)/(6*h(i))
+    s += (y[i-1] - (M[i-1]*(h(i))**2)/6)*((x[i]-point)/h(i))
+    s += (y[i] - (M[i]*(h(i))**2)/6)*((point-x[i-1])/h(i))
     return s
     
     
 def find_C(x,y):
-    C = [0] * (len(x)-1)
-    d = [0] * (len(x)-1)
-    for i in range(len(x)-1):
-        C[i] = [0] * (len(x))
+    C = [0] * (len(x))
+    d = [0] * (len(x) - 2)
+    for i in range(len(x)):
+        C[i] = [0] * (len(x) - 1)
     
-    for i in range(1, len(x)-1):
-        for j in range(len(x)):
+    for i in range(1, len(x) - 1):
+        for j in range(1, len(x)):
             if i == j:
-                h1 = x[i] - x[i-1]
-                h2 = x[i+1] - x[i]
-                C[i][j] = (h1+h2)/3
+                C[i][j-1] = (h(i)+h(i+1))/3
             elif j == i + 1:
-                h = x[i+1] - x[i]
-                C[i][j] = h/6
+                C[i][j-1] = h(i+1)/6
             elif j == i - 1:
-                h = x[i] - x[i-1]
-                C[i][j] = h/6
+                C[i][j-1] = h(i)/6
             elif abs(j-1) > 1:
-                C[i][j] = 0
+                C[i][j-1] = 0
     for i in range(1,len(y)-1):
-        d[i-1] = ((y[i+1]-y[i])/(x[i+1] - x[i])) - ((y[i]-y[i-1])/x[i]-x[i-1])
-    return C,d
+        d[i-1] = ((y[i+1]-y[i])/(h(i+1))) - ((y[i]-y[i-1])/(h(i)))
+    return C[1:-1],d
 
 def init():
-    with open("F:/4 семак/вычмат/VichMat/------/Interpolation/Trigonometric/nums.txt", "r") as f:
+    with open("D:/vich/Interpolation/Trigonometric/nums.txt", "r") as f:
         nums = f.readlines()
         n = len(nums)
         x = [0] * n
