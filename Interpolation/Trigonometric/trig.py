354454d78885ff42acb0c0aa0ce6c5a158729bb8
Vadim Glinskiy <v.glinskiy@icloud.com>
2024-04-18 20:40:22 +0700

    еее

--- Interpolation/Trigonometric/trig.py
+++ Interpolation/Trigonometric/trig.py
@@ -2,24 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 point = 1.5
-x0 = 0
+x0 = 1
 h = 1
 n = 4
-coef = 10
+coef = 1
 
 def main():
-    
     x,y = init(x0, h, n)
     print(f"{x}\n{y}")
     print(find_y(point, x0, n, h, y))
     graph(x,y)
 
 def graph(x, y):
-    s = [0] * int((n-1) * coef)
+    s = [0] * int((n) * coef)
     i = 0
-    re = [0] * int((n-1) * coef)
-    im = [0] * int((n-1) * coef)
-    y_orig = [0] * int((n-1) * coef)
+    re = [0] * int((n) * coef)
+    im = [0] * int((n) * coef)
+    y_orig = [0] * int((n) * coef)
     for i in range(len(s)):
         s[i] = round((x[0] + 1 * (h/2) + h*i) / coef, 4)
         xx = find_y(s[i], x0, n, h, y)
@@ -30,22 +29,26 @@ def graph(x, y):
         
     
 
-    figure, axis = plt.subplots(2,2)
+    # figure, axis = plt.subplots(2,2)
     
     
-    figure.set_size_inches(9,7)
+    # figure.set_size_inches(9,7)
     
-    axis[0,0].plot(re,im)
-    axis[0,0].set_title('Re/Im')
+    # axis[0,0].plot(re,im)
+    # axis[0,0].set_title('Re/Im')
     
-    axis[0,1].plot(s,re)
-    axis[0,1].set_title('x/Re')
+    # axis[0,1].plot(s,re)
+    # axis[0,1].set_title('x/Re')
     
-    axis[1,0].plot(s,im)
-    axis[1,0].set_title('x/Im')
+    # axis[1,0].plot(s,im)
+    # axis[1,0].set_title('x/Im')
     
-    axis[1,1].plot(s,y_orig)
-    axis[1,1].set_title('Original')
+    # axis[1,1].plot(s,y_orig)
+    # axis[1,1].set_title('Original')
+
+    plt.plot(x,y,'ro')
+    plt.plot(s,re,'b')
+    plt.plot(s,im,'g')
     
     plt.show()
         
@@ -75,8 +78,10 @@ def find_y(x, x0, n, h, y):
     yx = 0
     i = 1j
     for j in range(int(-n/2)+1, int(n/2)+1):
-        #yx += A(j,y,n) * (round(np.cos(2*np.pi*j*((x-x0)/(n*h))), 4) + i * round(np.sin(2*np.pi*j*((x-x0)/(n*h))), 4))
-        yx += A(j,y,n) * np.exp(2 * np.pi * i * j * ((x-x0)/(n*h)))
+        yx += round(np.cos(2*np.pi*j*((x-x0)/(n*h))), 4)
+        yx += i * round(np.sin(2*np.pi*j*((x-x0)/(n*h))), 4)
+        yx *= A(j,y,n)
+        # yx += A(j,y,n) * np.exp(2 * np.pi * i * j * ((x-x0)/(n*h)))
         #print(A(j,y,n) * (round(np.cos(2*np.pi*j*((x-x0)/(n*h))), 4) + i * round(np.sin(2*np.pi*j*((x-x0)/(n*h))), 4)))
     yx *= 1/n
     return np.round(yx, 4)
