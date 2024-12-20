16e0035ad9f5fd17cd4d83d0f703727bad8702f2
Vadim Glinskiy <v.glinskiy@.com>
2024-12-20 18:34:48 +0700

    Изменение под условия домашки

--- Interpolation/Chisl/Trap_and_Sims.py
+++ Interpolation/Chisl/Trap_and_Sims.py
@@ -5,31 +5,44 @@ n = 10
 h = 0.1
 def main():
     print(trap(func_trap, h, n, x0))
-    print(Simpson(func_simps, x0, h, n))
+    #print(Simpson(func_simps, x0, h, n))
 
 
 def trap(f, h,n,x0):
-    x = np.linspace(x0, x0 + (n-1)*h, n)
-    y = f(x)
-    integral = (y[0] + y[-1])/2
-    for i in range(1,n):
-        integral += y[i]
-    integral *= h
-    return integral
+    x = np.round(np.linspace(x0, x0 + (n)*h, n+1), 4)
+    y = np.round(f(x), 4)
+    res = 0
+    for i in range(0,len(x)-1,1):
+        res += integral_T(x,y,i)    
+    return round(res,4)
 
 
 def Simpson(f,x0,h,n):
     if n % 2 != 0:
         raise ValueError('n должно быть четным')
-    x = np.linspace(x0, x0 + (n-1)*h, n)
-    y = f(x)   
-    integral = h/3 * (y[0] + 4*np.sum(y[1:-1:2]) + 2*np.sum(y[2:-1:2]) + y[-1])
+    res = 0
+    x = np.linspace(x0, x0 + (n)*h, n+1)
+    x= np.round(x,4)
+    y = f(x)
+    y = np.round(y,4)
+    for i in range(0,len(x)-2,2):
+        res += integral_S(x,y,i)
+        
+    return res
 
-    return h/3 * (y[0] + y[-1] + 4 * np.sum(y[1:-1:2]) + 2 * np.sum(y[2:-1:2]))
+def integral_S(x,y,i):
+    print(f"{int(i/2 + 1)}.  ", round((x[i+2]-x[i])*(1/6 * y[i] + 2/3*y[i+1] + 1/6*y[i+2]),4))
+    #print(f"({x[i+2]}-{x[i]})*(1/6 * {y[i]} + 2/3*{y[i+1]} + 1/6*{y[i+2]})")
+    return round((x[i+2]-x[i])*(1/6 * y[i] + 2/3*y[i+1] + 1/6*y[i+2]),4)
 
+def integral_T(x,y,i):
+    a = (x[i+1]-x[i])*(1/2 * y[i] + 1/2*y[i+1])
+    print(f"{int(i + 1)}.  ", round(a,4))
+    # print(f"({x[i+1]}-{x[i]})*(1/2 * {y[i]} + 1/2*{y[i+1]})")
+    return a
 
 def func_simps(x):
-    return np.round(x**2 + 3*x ,4)
+    return 1/x
 
 def func_trap(x):
     return 1/x
